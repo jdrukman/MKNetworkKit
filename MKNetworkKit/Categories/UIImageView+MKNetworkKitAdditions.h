f888bcbd75549e30c819b87cb9a757cda54b31d7
Jon Drukman <jsd@beatsmusic.com>
2014-08-11 13:35:15 -0700

    attempt to get podspec to play ball

--- MKNetworkKit/Categories/UIImageView+MKNetworkKitAdditions.h
+++ MKNetworkKit/Categories/UIImageView+MKNetworkKitAdditions.h
@@ -22,8 +22,9 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
-
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#endif
 
 extern const float kFromCacheAnimationDuration;
 extern const float kFreshLoadAnimationDuration;
